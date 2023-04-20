7d3f308802054950deaf9fcff4bd6550aa15d7a5
Burenkov Serge <uzaren@ukr.net>
2023-04-20 23:30:19 +0300

    Демо версия

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+const startButton = document.querySelector("button[data-start]");
+const stopButton = document.querySelector("button[data-stop]");
+const bodY = document.querySelector("body");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timeout = 0;
+
+startButton.addEventListener("click", startColorChange);
+startButton.classList = "btn";
+stopButton.classList = "btn";
+
+stopButton.addEventListener("click", stopColorChange);
+
+function startColorChange() {
+  bodY.style.backgroundColor = getRandomHexColor();
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timeout = setTimeout(startColorChange, 1000);
+  console.log("timeout:", timeout);
+}
+
+function stopColorChange() {
+  clearTimeout(timeout);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    startFunction();
+  },
+};
+
+const starT = document.querySelector('button[data-start]');
+starT.disabled = true;
+// document.querySelector("#datetime-picker").value = "2024-01-01 00:00:00";
+
+flatpickr('#datetime-picker', options);
+
+function startFunction() {
+  starT.disabled = false;
+  starT.textContent = 'Start';
+  starT.addEventListener('click', countdown);
+}
+// startFunction();
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+// console.log("date:", date);
+
+// const specifiedDate = new Date("2024-01-01 00:00:00");
+
+// console.log(specifiedDate.getTime());
+
+// const specifiedDate = dataFromInput;
+
+function countdown() {
+  starT.textContent = 'Stop';
+  starT.removeEventListener('click', countdown);
+  const dataFromInput = document.querySelector('#datetime-picker').value;
+  let specifiedDate = Date.parse(dataFromInput);
+  starT.addEventListener('click', () => {
+    clearTimeout(timeout);
+    startFunction();
+  });
+
+  let dateNow = new Date(),
+    // date = specifiedDate.getTime() - dateNow.getTime(),
+    date = specifiedDate - dateNow.getTime(),
+    timeout = 0;
+
+  let { days, hours, minutes, seconds } = convertMs(date);
+
+  if (date <= 0) return alert('Please choose a date in the future!');
+
+  document.querySelector('span[data-days]').innerHTML = days;
+
+  if (hours < 10) hours = '0' + hours;
+  document.querySelector('span[data-hours]').innerHTML = hours;
+
+  if (minutes < 10) minutes = '0' + minutes;
+  document.querySelector('span[data-minutes]').innerHTML = minutes;
+
+  if (seconds < 10) seconds = '0' + seconds;
+  document.querySelector('span[data-seconds]').innerHTML = seconds;
+
+  timeout = setTimeout(countdown, 1000);
+}
