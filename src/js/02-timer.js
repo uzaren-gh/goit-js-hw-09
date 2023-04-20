c952c450957b3d9a71a464313358adefd91b2f46
Burenkov Serge <uzaren@ukr.net>
2023-04-20 23:35:42 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,6 +15,7 @@ const options = {
 const starT = document.querySelector('button[data-start]');
 starT.disabled = true;
 // document.querySelector("#datetime-picker").value = "2024-01-01 00:00:00";
+// лорлр
 
 flatpickr('#datetime-picker', options);
 
