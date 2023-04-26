860e44e5ce9c835cc195efdb0d892ec16eeadd18
uzaren-gh <125304584+uzaren-gh@users.noreply.github.com>
2023-04-26 21:09:53 +0000

    Deploying to gh-pages from @ uzaren-gh/goit-js-hw-09@1ac0b2067527d898265ed65cc2f39d26e6862c32 🚀

---
+++ 01-color-switcher.a558a5cd.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");var d=0;e.disabled=!0,t.addEventListener("click",(function a(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1,d=setTimeout(a,1e3),console.log("timeout:",d)})),e.addEventListener("click",(function(){clearTimeout(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.a558a5cd.js.map
